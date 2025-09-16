7bee4ffb8b8467365d1105cdf3a81ed0faea0976
LordVillain <danyailyinchyk2005@gmail.com>
2025-09-16 19:17:34 +0300

    исправил баг при котором контейнер не запускался

--- cmd/server.go
+++ cmd/server.go
@@ -1,17 +1,21 @@
-package cmd
+package main
 
 import (
+	"fmt"
+
 	"github.com/LordVillain/Recommendation-service/internal/recommendation"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	fmt.Println("Starting recommendation service...")
 
 	mlURL := "http://localhost:8000" // Python ML сервис
     
 	recSvc := recommendation.NewRecommendationService(mlURL)
 	recommendation.NewRecommendationHandler(r, recSvc)
 
-	r.Run(":8080")
+	r.Run(":8086")
+
 }
